bfdbe5ad02d2943a4000c061257f406fee40bebc
ADAMpoteto <taka87hiro51@gmail.com>
2026-09-02 22:36:43 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,3 +5,4 @@ const URL_ACT  = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRayAxX6Aex3K-
 const URL_TRIP = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRayAxX6Aex3K-PPMF5w_5al4vB1Ng_0DHRPCsrXaBcQE6q9WTiNoNuLQIz0MtFgZerMKZzG0ywHGGq/pub?gid=177122507&single=true&output=csv";
 const URL_MH   = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRayAxX6Aex3K-PPMF5w_5al4vB1Ng_0DHRPCsrXaBcQE6q9WTiNoNuLQIz0MtFgZerMKZzG0ywHGGq/pub?gid=1521643916&single=true&output=csv";
 const URL_CYCLE= "https://docs.google.com/spreadsheets/d/e/2PACX-1vRayAxX6Aex3K-PPMF5w_5al4vB1Ng_0DHRPCsrXaBcQE6q9WTiNoNuLQIz0MtFgZerMKZzG0ywHGGq/pub?gid=587788443&single=true&output=csv";
+const URL_MICHINOEKI = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRayAxX6Aex3K-PPMF5w_5al4vB1Ng_0DHRPCsrXaBcQE6q9WTiNoNuLQIz0MtFgZerMKZzG0ywHGGq/pub?gid=1896265159&single=true&output=csv";
